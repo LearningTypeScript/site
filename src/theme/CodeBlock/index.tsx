dd34ecfbfa820c91cc2195c1e1cdd228770f503d
Josh Goldberg <git@joshuakgoldberg.com>
2023-02-16 18:46:58 -0500

    fix: align manual CodeBlock styles to shiki's (#105)

---
+++ src/theme/CodeBlock/index.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import React from "react";
+import CodeBlock from "@theme-original/CodeBlock";
+
+import styles from "./styles.module.css";
+
+export type CodeBlockWrapperProps = React.HTMLAttributes<HTMLPreElement>;
+
+export default function CodeBlockWrapper({
+  children,
+  className,
+  ...props
+}: CodeBlockWrapperProps) {
+  return (
+    <>
+      <CodeBlock
+        className={clsx("shiki hihi", styles.codeBlock, className)}
+        {...props}
+      >
+        <div className="code-container line">
+          <code>{children}</code>
+        </div>
+      </CodeBlock>
+    </>
+  );
+}
