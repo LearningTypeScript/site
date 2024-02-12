e67a326cf44c1d1e3099983c3262bb6d343fd7d9
Josh Goldberg <git@joshuakgoldberg.com>
2024-02-12 12:28:49 -0500

    feat: added article on Branded Types

--- src/theme/ArticlesSection/index.tsx
+++ src/theme/ArticlesSection/index.tsx
@@ -1,49 +1,10 @@
 import { useArticles } from "@site/src/utils/useArticles";
 import React from "react";
-import { Card, CardProps } from "../Card";
+import { Card } from "../Card";
 import { CardsArea } from "../CardsArea";
 import { MainArea } from "../MainArea";
 
 import styles from "./styles.module.css";
-
-const articles: CardProps[] = [
-  {
-    description: "Lorem ipsum typesum TypeScript etc. etc. etc.",
-    href: "/article/analyzing-dna",
-    meta: "01. Example Article",
-    title: "Example Article",
-  },
-  {
-    description: "Lorem ipsum typesum TypeScript etc. etc. etc.",
-    href: "/article/analyzing-dna2",
-    meta: "02. Example Article",
-    title: "Example Article",
-  },
-  {
-    description: "Lorem ipsum typesum TypeScript etc. etc. etc.",
-    href: "/article/analyzing-dna3",
-    meta: "03. Example Article",
-    title: "Example Article",
-  },
-  {
-    description: "Lorem ipsum typesum TypeScript etc. etc. etc.",
-    href: "/article/analyzing-dna4",
-    meta: "04. Example Article",
-    title: "Example Article",
-  },
-  {
-    description: "Lorem ipsum typesum TypeScript etc. etc. etc.",
-    href: "/article/analyzing-dna5",
-    meta: "05. Example Article",
-    title: "Example Article",
-  },
-  {
-    description: "Lorem ipsum typesum TypeScript etc. etc. etc.",
-    href: "/article/analyzing-dna6",
-    meta: "06. Example Article",
-    title: "Example Article",
-  },
-];
 
 export const ArticlesSection = () => {
   const articles = useArticles();
@@ -55,14 +16,11 @@ export const ArticlesSection = () => {
         description="Top tips and tricks for why TypeScript behaves the way it does, and how you can work effectively with it."
         heading="Articles"
         link={{
-          children:
-            articles.length === 1
-              ? "See all articles"
-              : `See all ${articles.length} articles`,
+          children: `See all ${articles.length} articles`,
           href: "/articles",
         }}
       >
-        {articles.map((article) => (
+        {articles.slice(0, 3).map((article) => (
           <Card
             className={styles.card}
             description={article.description}
